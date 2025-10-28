c8f66d268186fef3612a0cede1e9c5a58ba2ec6e
William Aguas <148153196+WILLIAMAGUAS@users.noreply.github.com>
2025-10-28 04:02:16 +0000

    primer aporte

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,60 @@
-import React from "react";
-
+import React, { useState, useEffect } from 'react';
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	let [tarea, setTarea] = useState("")
+	let [lista, setLista] = useState([])
+
+
+
+	const traerTareas = () => {
+		 fetch ("https://playground.4geeks.com/todo/users/william")
+    .then(response => {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        // Lee la respuesta como JSON
+        return response.json();
+    })
+    .then(data => {
+        // Haz lo que quieras con la respuesta JSONificada
+        setLista(data.todos)
+	
+    })
+    .catch(error => {
+        console.log('Looks like there was a problem: \n', error);
+    });
+	}
+	useEffect(() => {
+    traerTareas();
+  	},[]);
+	const escribirTarea = (event) => {
+		setTarea(event.target.value)
+	}
+	const agregar = (event) => {
+		if (event.key === "Enter") {
+			
+		}
+	}
+	const eliminarTarea = (posicion) => {
+		setLista(lista.filter((item, index) => index !== posicion))
+	}
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="contenedor d-flex justify-content-center align-items-center">
+			<h1>Todolist </h1>
+			<div className="paper border text-center">
+				<input type="text" placeholder="agrega una tarea" onChange={escribirTarea} value={tarea} onKeyDown={agregar} />
+				<ul className="list-unstyled">
+					{lista.map((item, index) => (
+						<li key={index}>{item.label}<span className="borrador" onClick={() => eliminarTarea(index)}> ❌ </span> <hr /> </li>
+					))}
+				</ul>
+				<p className="d-flex justify-content-start">{lista.length} item left</p>
+			</div >
 		</div>
-	);
-};
+	)
 
+}
 export default Home;
